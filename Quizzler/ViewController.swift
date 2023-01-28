cf38b43eddd4f192dde0663ae9c96f47afaf19ab
Janibek <jano.vr@mail.ru>
2023-01-28 12:26:00 +0400

    Initial Commit

---
+++ Quizzler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quizzler
+//
+//  Created by Janibek Voskanyan on 28.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
