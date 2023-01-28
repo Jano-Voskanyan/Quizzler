7ab1732c2269322e06a6c545318921e81b06a9ff
Janibek <jano.vr@mail.ru>
2023-01-28 17:47:16 +0400

    check the answers + commenting

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -14,26 +14,42 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     
+    //create an array of question and answer
     let quiz = [
-        "Four + Two is equal to Six",
-        "Five - Three is greater than One",
-        "Three + Eight is less than Ten"
+        ["4 + 2 = 6", "True"],
+        ["5 - 3 > 1", "True"],
+        ["3 + 8 < 10", "False"]
     ]
     
     var questionNumber = 0
     
+    //The code written here works when the application is launched.
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
     }
 
     @IBAction func answerButtonPressed(_ sender: UIButton) {
-        questionNumber += 1
+        
+        let userAnswer = sender.currentTitle //True, False
+        let actualAnswer = quiz[questionNumber][1]
+        
+        if userAnswer == actualAnswer {
+            print("Right!")
+        } else {
+            print("Wrong!")
+        }
+        
+        if questionNumber + 1 < quiz.count { //questionNumber reaches up to 2
+            questionNumber += 1
+        } else { //when questionNumber is equal to 3, questions are repeated
+            questionNumber = 0
+        }
         updateUI()
     }
     
-    func updateUI() {
-        questionLabel.text = quiz[questionNumber]
+    func updateUI() { //when starting, it asks questions in turn
+        questionLabel.text = quiz[questionNumber][0]
     }
 }
 
